5c31eb2e07c9f5e66a63d158ef41fc85c8a9cf28
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-27 02:13:44 +0330

    feat: authenticate user

--- monitor/authenticate.py
+++ monitor/authenticate.py
@@ -1,7 +1,4 @@
-from rest_framework.response import Response
 from authentications.jwt import decode_jwt
-from rest_framework import status
-
 
 def get_user(request):
     token = request.headers.get('Authorization')
@@ -11,6 +8,6 @@ def get_user(request):
         if user:
             return user.id
         else:
-            return Response({"message":"You are not authorized to view this endpoint"}, status=status.HTTP_401_UNAUTHORIZED)        
-        
-    return Response({"message":"You are not authorized to view this endpoint"}, status=status.HTTP_401_UNAUTHORIZED)
+            return None
+    else:
+        return None
