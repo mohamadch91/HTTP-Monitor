7474f0b50b599d8677da1be19371ae70ec9addcc
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-27 04:07:54 +0330

    fix: typo in urlpatters

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import LoginView, RegisterView
-url_patterns = [
-    path('/login', LoginView.as_view()),
-    path('/register', RegisterView.as_view()),
+urlpatterns = [
+    path('/login', LoginView.as_view(), name='Login'),
+    path('/register', RegisterView.as_view() ,name='Sign up'),
 ]
     
