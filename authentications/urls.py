9d7ff1c297ebec9c38e10f4cc396bc4749549197
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-27 03:59:34 +0330

    feat: complete URLS

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import LoginView, RegisterView
+url_patterns = [
+    path('/login', LoginView.as_view()),
+    path('/register', RegisterView.as_view()),
+]
+    
